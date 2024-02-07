2ba099e075d8a339fbcf054e6e4e1fbb60d517da
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-02-07 13:52:43 -0300

    configurando a api para lidar com json

--- src/server.js
+++ src/server.js
@@ -3,5 +3,6 @@ const express = require('express');
 const PORT = 3333;
 
 const app = express();
+app.use(express.json());
 
 app.listen(PORT, () => console.log(`the app is running on the door ${PORT}`));
