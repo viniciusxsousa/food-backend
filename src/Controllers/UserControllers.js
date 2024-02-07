bc55e530ab531c1898b3ed41f14b9beb391cf81e
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-02-07 18:38:59 -0300

    inserindo o status code

--- src/Controllers/UserControllers.js
+++ src/Controllers/UserControllers.js
@@ -3,7 +3,7 @@ class UserControllers {
     create(req, res) {
         const { name, email } = req.body;
 
-        res.send(`O usuário ${name} foi cadastrado com o e-mail ${email}`);
+        res.status(201).send(`O usuário ${name} foi cadastrado com o e-mail ${email}`);
     }
 
 }
