d23bb5c728b595b7cb488c4db522273d55ac6a0f
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-02-07 21:26:13 -0300

    configurando o servidor para lidar com erros

--- src/Controllers/UserControllers.js
+++ src/Controllers/UserControllers.js
@@ -1,7 +1,13 @@
+const AppError = require("../utils/AppError");
+
 class UserControllers {
 
     create(req, res) {
         const { name, email } = req.body;
+
+        if(!name) {
+            throw new AppError("Nome é obrigatório.");
+        }
 
         res.status(201).send(`O usuário ${name} foi cadastrado com o e-mail ${email}`);
     }
--- src/server.js
+++ src/server.js
@@ -1,4 +1,8 @@
+require('express-async-errors');
+
 const express = require('express');
+
+const AppError = require('./utils/AppError');
 
 const routes = require('./routes');
 
@@ -8,5 +12,23 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use((error, request, response, next) => {
+
+    if(error instanceof AppError) {
+        return response.status(error.statusCode).json({
+            status: "Error",
+            message: error.message
+        })
+    }
+
+    console.error(error);
+
+    return response.status(500).json({
+        status: "Error",
+        message: "Error Internal Server"
+    })
+
+})
 
 app.listen(PORT, () => console.log(`the app is running on the door ${PORT}`));
