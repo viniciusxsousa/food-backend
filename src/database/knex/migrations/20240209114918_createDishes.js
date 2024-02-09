fb4c3eacc01c724ab7cc6f5822112f548f72be8b
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-02-09 09:22:45 -0300

    criando a migrate para pratos

---
+++ src/database/knex/migrations/20240209114918_createDishes.js
@@ -0,0 +1,13 @@
+exports.up = knex => knex.schema.createTable("Dishes", table => {
+    table.increments("id");
+    table.text("name");
+    table.text("description");
+    table.integer("category").references("id").inTable("categories");
+    table.float("price");
+    table.text("picture").default(null);
+    table.timestamp("created_at").default(knex.fn.now());
+    table.timestamo("updated_at").default(knex.fn.now());
+})
+
+
+exports.down = knex => knex.schema.dropTable("Dishes")
