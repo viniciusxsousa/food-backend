7dad7e9f1583b2d09e1ac9c9905b036f9626773a
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-02-09 20:07:54 -0300

    criando a estrutura do controller

---
+++ src/Controllers/DishesControllers.js
@@ -0,0 +1,12 @@
+const AppError = require('../utils/AppError');
+const knex = require('../database/knex');
+
+class DishesControllers {
+
+    async create(req, res){
+
+    }
+
+}
+
+module.exports = DishesControllers
--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -1,5 +1,11 @@
 const { Router } = require("express");
 
+const DishesControllers = require('../Controllers/DishesControllers');
+
 const dishesRoutes = Router();
+
+const dishesControllers = new DishesControllers();
+
+dishesRoutes.post('/', dishesControllers.create);
 
 module.exports = dishesRoutes;
