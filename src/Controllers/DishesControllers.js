af2048d555a64caf739217cf2bb73c5dc273e641
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-02-09 20:40:09 -0300

    criando a função de cadastrar prato

--- src/Controllers/DishesControllers.js
+++ src/Controllers/DishesControllers.js
@@ -4,7 +4,24 @@ const knex = require('../database/knex');
 class DishesControllers {
 
     async create(req, res){
+        const {name, description, category, price} = req.body;
 
+        if(!name) {
+            throw new AppError("O nome do prato deve ser informado.");
+        }
+
+        if(!price) {
+            throw new AppError("O valor do prato deve ser informado.");
+        }
+
+        await knex('dishes').insert({
+            name,
+            description,
+            category,
+            price
+        })
+
+        res.status(201).json({name, description, category, price});
     }
 
 }
