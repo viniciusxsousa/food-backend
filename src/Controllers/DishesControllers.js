a4e4d20a30fa984c0c1cc1fa91d1256391912a16
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-02-09 21:07:20 -0300

    inserindo no banco os ingredientes dos pratos

--- src/Controllers/DishesControllers.js
+++ src/Controllers/DishesControllers.js
@@ -4,7 +4,7 @@ const knex = require('../database/knex');
 class DishesControllers {
 
     async create(req, res){
-        const {name, description, category, price} = req.body;
+        const {name, description, category, price, ingredients} = req.body;
 
         if(!name) {
             throw new AppError("O nome do prato deve ser informado.");
@@ -14,12 +14,22 @@ class DishesControllers {
             throw new AppError("O valor do prato deve ser informado.");
         }
 
-        await knex('dishes').insert({
+        const [dished] = await knex('dishes').insert({
             name,
             description,
             category,
             price
         })
+
+
+        const ingredientsList = ingredients.map((ingredient) => {
+            return {
+                name: ingredient,
+                dishe_id: dished
+            }
+        })
+
+        await knex("ingredients").insert(ingredientsList);
 
         res.status(201).json({name, description, category, price});
     }
