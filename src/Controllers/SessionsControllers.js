061702fc913741bb8879bdf2a90c363298f74c20
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-02-21 14:31:50 -0300

    verificando se usuário existe ao fazer login

--- src/Controllers/SessionsControllers.js
+++ src/Controllers/SessionsControllers.js
@@ -1,9 +1,18 @@
+const knex = require('../database/knex/index');
+const AppError = require('../utils/AppError');
+
 class SessionsControllers {
 
     async create(req, res) {
         const { email, password } = req.body;
 
-        return res.json({email, password});
+        const user = await knex('users').where({email}).first();
+
+        if(!user) {
+            throw new AppError('E-mail ou senha inválido.', 401);
+        }
+
+        return res.json({user});
     }
 
 }
