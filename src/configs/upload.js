656c60775aa11c9d7ebec2691a5e0cd2b47c0384
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-02-26 21:59:45 -0300

    cofigindo erroa na declaração do path

--- src/configs/upload.js
+++ src/configs/upload.js
@@ -2,8 +2,8 @@ const path = require('path');
 const multer = require('multer');
 const crypto = require('crypto');
 
-const TMP_FOLDER = path(__dirname, '..', '..', 'tmp');
-const UPLOADS_FOLDER = path(TMP_FOLDER, 'uploads' );
+const TMP_FOLDER = path.resolve(__dirname, '..', '..', 'tmp');
+const UPLOADS_FOLDER = path.resolve(TMP_FOLDER, 'uploads' );
 
 const MULTER = {
     storage: multer.diskStorage({
