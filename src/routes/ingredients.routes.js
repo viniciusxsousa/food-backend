f160d5fd1f6252d8fb2f61326643dba6196df8ed
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-03-14 20:07:23 -0300

    aplicando o middleware de autenticação nas rotas de igrediente

--- src/routes/ingredients.routes.js
+++ src/routes/ingredients.routes.js
@@ -2,9 +2,12 @@ const { Router } = require('express');
 const IngredientsControllers = require('../Controllers/IngredientsControllers');
 
 const verifyUserAuthorization = require('../middlewares/verifyUserAuthorization');
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
 
 const ingredientsRouter = Router();
 const ingredientsControllers = new IngredientsControllers();
+
+ingredientsRouter.use(ensureAuthenticated);
 
 ingredientsRouter.post('/', verifyUserAuthorization('admin'), ingredientsControllers.create);
 ingredientsRouter.delete('/:id', verifyUserAuthorization('admin'), ingredientsControllers.delete);
