c050aa5324b80b55fa3dd4109387cb38c39e3fa9
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-03-14 11:33:00 -0300

    refatorando a tabela users para trabalhar com permissões

--- src/database/knex/migrations/20240208233815_createUsers.js
+++ src/database/knex/migrations/20240208233815_createUsers.js
@@ -3,7 +3,7 @@ exports.up = knex => knex.schema.createTable("users", table => {
     table.text("name");
     table.text("email");
     table.text("password");
-    table.text("rule").default("user");
+    table.enum("rule", ["admin", "user"], { useNative: true, enumName: "roles" }).notNullable().default('user');
     table.timestamp("created_at").default(knex.fn.now());
     table.timestamp("updated_at").default(knex.fn.now());
 });
